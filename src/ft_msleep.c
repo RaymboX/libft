376a51b51040aef5113059b3a9e32ec793f02eab
mraymond <quadrayxl@gmail.com>
2022-09-01 10:20:14 -0400

    SCHOOL: correction name of ft_is_int_range and remove var in msleep

--- src/ft_msleep.c
+++ src/ft_msleep.c
@@ -6,7 +6,7 @@
 /*   By: mraymond <mraymond@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 10:15:50 by mraymond          #+#    #+#             */
-/*   Updated: 2022/09/01 10:16:14 by mraymond         ###   ########.fr       */
+/*   Updated: 2022/09/01 10:19:46 by mraymond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ static long unsigned int	now_millisecond(void)
 void	ft_msleep(long unsigned int wait_millisecond)
 {
 	long unsigned int	start;
-	long unsigned int	how_long;
 
 	start = now_millisecond();
 	while (now_millisecond() < wait_millisecond + start)
